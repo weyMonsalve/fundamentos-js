b25885d1ee34f53b7d3509affc0c53787cf29e18
wbeimar <wbeimarmonsalve33@gmail.com>
2025-02-13 07:23:59 -0500

    Tipos de varables

---
+++ anatomia-de-una-variable/clase-variables.js
@@ -0,0 +1,17 @@
+let cajaDeWbeimar = "goku";
+console.log(cajaDeWbeimar);
+
+// LO NO PERMITIDO
+// let c = "goku";
+// let cda = "goku";
+// let pcWbeimar = "goku";
+// NO SE PERMITE O NO ESTA BIEN UTILIZADO YA QUE SERIAN AMBIGUEDADES O NO SERIA DESCRIPTIVO 
+// PARA OTRAS PERSONAS INCLUYENDO OTROS DESARROLLADORES
+
+// LO PERMITIDO
+
+// let primerTrasteoDeWbeimar = "goku";
+// let urlDelUsuario = "https://www.google.com";
+// let idDelUsuario = 1;
+
+// ESTO YA ES PERMITIDO POR QUE ESTAMOS DANDO ALGO EN CONCRETO Y DESCRIPTIVO PARA EL LOS DESARROLLADORES
