3eb5560091fb8c644dd20486b62b09edd74accee
wbeimar <wbeimarmonsalve33@gmail.com>
2025-02-13 07:52:02 -0500

    condicinal if

---
+++ ejecucion-condicional-if/condicional-if.js
@@ -0,0 +1,35 @@
+// La estructura de control if en JavaScript se usa para ejecutar un bloque de 
+//código solo si se cumple una condición específica. Es una de las estructuras de control más 
+//básicas y utilizadas en programación.
+
+// Sintaxis básica
+
+// if (condición) {
+//     // Código a ejecutar si la condición es verdadera
+// }
+
+// let nombre = "Luciana";
+
+// if (nombre === "Luciana") {
+//     console.log("Hola, Luciana");
+// }
+
+
+// let nombre1 = "Matias";
+
+// if (nombre1 === "Luciana") {
+//     console.log("hola, matias");
+// } else {
+//     console.log("Nombre no encontrado");
+// }
+
+
+// let nombre2 = "Matias";
+
+// if (nombre2 === "Luciana") {
+//     console.log("hola, Luciana");
+// } else if (nombre2 === "Matias") {
+//     console.log("Hola Matias");
+// } else {
+//     console.log("Nombre no encontrado");
+// }
