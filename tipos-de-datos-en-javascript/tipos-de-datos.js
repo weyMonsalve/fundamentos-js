d2e1fab998cb7553572fb205f5116d7818a8716e
wbeimar <wbeimarmonsalve33@gmail.com>
2025-02-13 07:28:23 -0500

    Tipos de datos en js

---
+++ tipos-de-datos-en-javascript/tipos-de-datos.js
@@ -0,0 +1,39 @@
+// PRIMITIVOS
+
+// string
+let nombre = 'wey'
+
+// number
+let edad = 40
+
+// boolean
+let mayorDeEdad = true
+
+// Null
+let noHayValor = null
+
+// undefined
+let noDefinido = undefined
+
+// symbol
+let simboloUnico = Symbol('unico')
+
+// bigin
+let numeroGrande = 2 n
+
+//***************************************************************************************** *
+
+// COMPLEJOS
+
+// objec
+let carro = {
+    marca: 'tesla',
+    modelo: 'model S'
+}
+
+// arrays
+let frutas = ['manzana', 'mango', 'bananas', 'uvas']
+
+
+// function
+function saludar() {}
